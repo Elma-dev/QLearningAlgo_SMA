07a6e618ca2061326ff8a39b92f5815126328cd4
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-06-06 21:37:54 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane root=new AnchorPane();
+        root.setPrefWidth(300);
+        root.setPrefHeight(300);
+        Rectangle rectangle[][]=new Rectangle[3][3];
+
+        Scene scene=new Scene(root);
+        stage.setScene(scene);
     }
 }
